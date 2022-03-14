077ca44198e6d3d02d39a5384a30f3fab28f57c7
pchroscicki <pchroscicki@gmail.com>
2022-03-14 18:17:14 +0100

    dashboard

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
 
 
 class IndexView(View):
@@ -9,3 +10,8 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+class Dashboard:
+
+    def dashboard(request):
+        return HttpResponse('dashboard.html')
--- scrumlab/urls.py
+++ scrumlab/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from jedzonko.views import IndexView
+from jedzonko.views import IndexView, Dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view()),
+    path('main/', Dashboard)
 ]
