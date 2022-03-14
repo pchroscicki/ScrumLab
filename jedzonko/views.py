59ac4fe76cae322d1a539bc50a6c6de9c6dbe9c8
nieonek <nieonek@gmail.com>
2022-03-14 18:04:25 +0100

    task1.1_1.2_1.3

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -8,4 +8,6 @@ class IndexView(View):
 
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
-        return render(request, "test.html", ctx)
+        return render(request, "index.html", ctx)   # zmiana z test.html
+
+
