68be8d8824264333b556f662b95e424ddf48a98b
pchroscicki <pchroscicki@gmail.com>
2022-03-16 15:13:45 +0100

    PC_5.1_models_update

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -1,5 +1,8 @@
+from enum import Enum
+
 from django.db import models
 import datetime
+
 
 # Create your models here.
 
@@ -17,10 +20,26 @@ class Schedule(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(null=True)
     created = models.DateTimeField(default=datetime.date.today)
-    updated = models.DateTimeField(default=datetime.date.today)
+    recipes = models.ManyToManyField(Recipe, through='RecipePlan')
 
 
+class DayName(models.Model):  # dayname choices by 'enume' Django build-in function
+    class Weekday(Enum):
+        Monday = 'Poniedziałek'
+        Tuesday = 'Wtorek'
+        Wednesday = 'Środa'
+        Thursday = 'Czwartek'
+        Friday = 'Piątek'
+        Saturday = 'Sobota'
+        Sunday = 'Niedziela'
+
+    name = models.CharField(choices=Weekday)
+    order = models.SmallIntegerField(unique=True)
 
 
-
-
+class RecipePlan(models.Model):
+    meal_name = models.CharField(max_length=256)  # (śniadanie, obiad itp)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    order = models.ForeignKey(DayName, to_fields=['order'], from_fields=['self'], on_delete=models.CASCADE)
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
